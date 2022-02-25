d2a092dd1a4f266d225e61cebd23292d517e1b23
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-02-25 13:14:46 +0100

    remove innecesary files and initial setup

--- src/app.tsx
+++ src/app.tsx
@@ -1,25 +1,4 @@
 import * as React from 'react';
-import styles from './mystyles.module.css';
-import logo from './public/logo_1.png';
-
 export const App = () => {
-  const [counter, setCounter] = React.useState<number>(0);
-  const handleIncrease = () => {
-    setCounter(counter + 1);
-  };
-  console.log(process.env.EXAMPLE_VARIABLE);
-  React.useEffect(() => {
-    console.log(counter);
-  }, [counter]);
-  return (
-    <>
-      <h1>Hello React and TS With Fast Refresh</h1>
-      <button onClick={handleIncrease}>Counter {counter}</button>
-      <div className={styles.redBackground}>RED</div>
-      <div id="img-container">
-        <img src={logo}></img>
-      </div>
-      ;
-    </>
-  );
+  return <h1>Memory Game</h1>;
 };
